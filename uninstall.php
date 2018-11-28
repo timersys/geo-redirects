bca857d0ed1100295d80aa9217e22e82db62f795
Damian <info@timersys.com>
2018-11-28 14:05:49 -0300

    fixed uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,31 +1,28 @@
 <?php
-
 /**
  * Fired when the plugin is uninstalled.
  *
- * When populating this file, consider the following flow
- * of control:
- *
- * - This method should be static
- * - Check if the $_REQUEST content actually is the plugin name
- * - Run an admin referrer check to make sure it goes through authentication
- * - Verify the output of $_GET makes sense
- * - Repeat with other user roles. Best directly by using the links/query string parameters.
- * - Repeat things for multisite. Once for a single site in the network, once sitewide.
- *
- * This file may be updated more in future version of the Boilerplate; however, this is the
- * general skeleton and outline for how the file should work.
- *
- * For more information, see the following discussion:
- * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
- *
- * @link       https://timersys.com
- * @since      1.0.0
- *
- * @package    Geotr
+ * @package    GeoTarget
  */
 
 // If uninstall not called from WordPress, then exit.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
+}
+$opts = get_option('geot_settings');
+
+// only uninstal regions/settings  If last plugin being uninstalled
+if( isset( $opts['geot_uninstall']) && '1' == $opts['geot_uninstall'] && ! function_exists('geot') ) {
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+	GeotFunctions\geot_uninstall();
+}
+// Run if this plugin is being uninstalled and uninstall is checked
+if( isset( $opts['geot_uninstall']) && '1' == $opts['geot_uninstall']  ) {
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+	GeotFunctions\uninstall([ 'geotr_cpt' ]);
+
+	// uncheck geot uninstall just in case after the plugin it's removed
+	$opts = get_option('geot_settings');
+	$opts['uninstall'] = '';
+	update_option( 'geot_settings', $opts);
 }
