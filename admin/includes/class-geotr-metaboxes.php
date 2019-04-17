b6ec8ae634bff70d3c249d11accac44a0e8b0c33
Damian <info@timersys.com>
2019-04-17 10:10:51 -0300

    new array nottations

--- admin/includes/class-geotr-metaboxes.php
+++ admin/includes/class-geotr-metaboxes.php
@@ -58,7 +58,7 @@ class Geotr_Metaboxes{
 		add_meta_box(
 			'geotr-rules',
 			 __( 'Redirection Rules', 'geotr' ),
-			array( $this, 'geotr_rules' ),
+			[ $this, 'geotr_rules' ],
 			'geotr_cpt',
 			'normal',
 			'core'
@@ -66,7 +66,7 @@ class Geotr_Metaboxes{
 		add_meta_box(
 			'geotr-opts',
 			 __( 'Redirection Options', 'geotr' ),
-			array( $this, 'geotr_opts' ),
+			[ $this, 'geotr_opts' ],
 			'geotr_cpt',
 			'normal',
 			'core'
@@ -120,7 +120,7 @@ class Geotr_Metaboxes{
 		// save box settings
 		update_post_meta( $post_id, 'geotr_options', apply_filters( 'geotr/metaboxes/sanitized_options', $opts ) );
 
-		$keys_geot = apply_filters('geotr/metaboxes/keys_geot', array('country', 'country_region', 'city', 'city_region', 'state', 'zip'));
+		$keys_geot = apply_filters('geotr/metaboxes/keys_geot', ['country', 'country_region', 'city', 'city_region', 'state', 'zip']);
 
 		// Start with rules
 		if( isset($_POST['geotr_rules']) && is_array($_POST['geotr_rules']) ) {
